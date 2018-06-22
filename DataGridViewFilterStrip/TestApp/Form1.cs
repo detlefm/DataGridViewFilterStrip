8a6ce9b06f689264603db6fa85b1c8d2a9e970cc
Detlef <meyer.detlef@gmail.com>
2018-06-22 10:13:29 +0200

    add comments

--- DataGridViewFilterStrip/TestApp/Form1.cs
+++ DataGridViewFilterStrip/TestApp/Form1.cs
@@ -22,6 +22,7 @@ namespace TestApp {
 
         private GridFilter<Person> GreaterName() {
             return new GridFilter<Person> {
+                // {HeaderText} is a placeholder and will replaced on runtime with the columns HeaderText
                 DisplayString = "{HeaderText} of Person > ",
                 Filter = new Func<IEnumerable<Person>, PropertyInfo, object, IEnumerable<Person>>(
                      delegate (IEnumerable<Person> src, PropertyInfo getter, object cmp) {
@@ -35,8 +36,12 @@ namespace TestApp {
         }
 
         private void Form1_Load(object sender, EventArgs e) {
+            // bind the FilterStrip to the dataGridView
             filterStrip = new FilterStrip<Person>(dataGridView1);
+            // if using user defined filters you can add an equal filter to all columns
+            // the equal filter is always present if no user defined filter for a column is defined
             filterStrip.AddStandardFilters();
+            // add the user defined filter
             filterStrip.AddFilter("Name", GreaterName());
             personBindingSource.DataSource = new ObjectBindingList<Person>(Person.CreateData());
         }
